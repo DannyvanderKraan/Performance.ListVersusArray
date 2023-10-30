4f4d4eb95ef979bfdb013f2cdeeff66486a92806
Danny van der Kraan <dvanderkraan@xpirit.com>
2023-10-30 17:19:49 +0100

    Add project files.

---
+++ Performance.ListVersusArray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+public class StringConcatenationBenchmark
+{
+    [Benchmark]
+    public List<int> UsingList()
+    {
+        List<int> numbersList = new List<int>(1000);
+        for (int i = 0; i < 1000; i++)
+        {
+            numbersList.Add(i);
+        }
+        return numbersList;
+    }
+
+    [Benchmark]
+    public int[] UsingArray()
+    {
+        int[] numbersArray = new int[1000];
+        for (int i = 0; i < 1000; i++)
+        {
+            numbersArray[i] = i;
+        }
+        return numbersArray;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var summary = BenchmarkRunner.Run<StringConcatenationBenchmark>();
+    }
+}
